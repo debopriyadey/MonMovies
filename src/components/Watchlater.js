9b961c89302f9a037f9f468068778715129776f7
debopriyadey <debodey158@gmail.com>
2021-03-26 13:18:22 +0530

    v1.02.2 updated the UI with typist

--- src/components/Watchlater.js
+++ src/components/Watchlater.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-
+import Typist from 'react-typist';
 
 import Navbar from './Navbar';
 import WatchlaterMovieCard from './WatchlaterMovieCard';
@@ -11,6 +11,7 @@ const useStyles = makeStyles((theme) => ({
     root: {
         overflow: 'hidden',
         margin: 0,
+        marginTop: "5%",
     },
 
     container: {
@@ -22,26 +23,49 @@ const useStyles = makeStyles((theme) => ({
         margin: '2%',
     },
 
+    // introText: {
+    //     color: "white",
+    //     margin: "30px",
+    //     height: "100px",
+    //     display: "flex",
+    //     alignItems: "flex-end",
+    // },
+
+    text: {
+        color: "white",
+        height: "70vh",
+        display: "grid",
+        alignItems: "center",
+        textAlign: "center",
+        fontFamily: "Bodoni MT Black",
+    },
+
 }));
 
 export default function Watchlater() {
     const classes = useStyles();
 
     const movies = useSelector(state => state.watchlater);
+    console.log(movies.length);
 
     return (
         <div className={classes.root}>
             <Navbar />
+            {/* <h1 className={classes.introText}> My Watchlater </h1> */}
             <div>
-                <Grid container spacing={3} className={classes.container}>
-                    {movies.length > 0 && (movies.map((movie) => (
-                        <Grid className={classes.card} key={movie.id}>
-                            <WatchlaterMovieCard movie={movie} />
-                        </Grid>
-                    ))
-                    )}
-                </Grid>
-
+                {movies.length === 0 ?
+                    <Typist>
+                        <h1 className={classes.text}> You have no movies saved in your Watchlater . </h1>
+                    </Typist> :
+                    <Grid container spacing={3} className={classes.container}>
+                        {movies.length > 0 && (movies.map((movie) => (
+                            <Grid className={classes.card} key={movie.id}>
+                                <WatchlaterMovieCard movie={movie} />
+                            </Grid>
+                        ))
+                        )}
+                    </Grid>
+                }
             </div>
         </div>
     )
--- src/components/watched.js
+++ src/components/watched.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-
+import Typist from 'react-typist';
 
 import Navbar from './Navbar';
 import WatchedMovieCard from './WatchedMovieCard';
@@ -11,6 +11,7 @@ const useStyles = makeStyles((theme) => ({
     root: {
         overflow: 'hidden',
         margin: 0,
+        marginTop: "5%",
     },
 
     container: {
@@ -20,6 +21,15 @@ const useStyles = makeStyles((theme) => ({
 
     card: {
         margin: '2%',
+    },
+
+    text: {
+        color: "white",
+        height: "70vh",
+        display: "grid",
+        alignItems: "center",
+        textAlign: "center",
+        fontFamily: "Bodoni MT Black",
     },
 
 }));
@@ -33,14 +43,19 @@ export default function Watched() {
         <div className={classes.root}>
             <Navbar />
             <div>
-                <Grid container spacing={3} className={classes.container}>
-                    {movies.length > 0 && (movies.map((movie) => (
-                        <Grid className={classes.card} key={movie.id}>
-                            <WatchedMovieCard movie={movie} />
-                        </Grid>
-                    ))
-                    )}
-                </Grid>
+                {movies.length === 0 ?
+                    <Typist>
+                        <h1 className={classes.text}> You have no movies saved in your Watched . </h1>
+                    </Typist> :
+                    <Grid container spacing={3} className={classes.container}>
+                        {movies.length > 0 && (movies.map((movie) => (
+                            <Grid className={classes.card} key={movie.id}>
+                                <WatchedMovieCard movie={movie} />
+                            </Grid>
+                        ))
+                        )}
+                    </Grid>
+                }
 
             </div>
         </div>
