2e65d14138b9f4497caa63b12e8cfb033c82e8e5
debopriyadey <debodey158@gmail.com>
2021-06-19 17:51:51 +0530

    routing solved by gourav

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Search from './components/Search';
 import Watched from './components/Watched';
 import Watchlater from './components/Watchlater';
 import  CurrentMovie from './components/CurrentMovie';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <Router basename="/MonMovies" >
+    <Router >
         <div className="App">
             <Switch>
-                <Route path="/MonMovies" exact component={Home} />
                 <Route path="/MonMovies/search" component={Search} />
-                <Route path="/MonMovies/watched" component={Watched} />
                 <Route path="/MonMovies/watchlater" component={Watchlater} />
+                <Route path="/MonMovies/watched" component={Watched} />
                 <Route path="/MonMovies/currentmovie" component={CurrentMovie} />
+                <Route path="/MonMovies" component={Home} />
+                <Route component={PageNotFound} />
             </Switch>
         </div>
     </Router>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -9,9 +10,11 @@ import App from './App';
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter basename="/MonMovies/">
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
